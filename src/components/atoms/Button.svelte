9b68ed524497578c815ac3c7e870a678724581e8
Aqua Minato <sweet4182691@gmail.com>
2021-12-21 07:18:06 +0900

    atoms/Buttonの追加

---
+++ src/components/atoms/Button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let text: string = 'Primary';
+	export let onClick: () => void = () => {};
+</script>
+
+<button
+	class="btn btn-primary bg-blue-500 rounded-3xl hover:bg-black duration-200"
+	on:click={onClick}
+>
+	<p class="my-2 mx-5 text-white">{text}</p>
+</button>
+
+<!-- <script>
+	import Button from '../components/atoms/Button.svelte';
+</script>
+
+<Button
+	onClick={() => {
+		console.log('click');
+	}}
+	text={'Button'}
+/> -->

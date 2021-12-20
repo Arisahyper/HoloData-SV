d85ba42137cae322e587fc572b29ab48dc6107ab
Aqua Minato <sweet4182691@gmail.com>
2021-12-21 08:59:34 +0900

    HeaderにLogoを追加、調節

--- src/components/organisms/Header.svelte
+++ src/components/organisms/Header.svelte
@@ -1,9 +1,18 @@
 <script>
 	import Title from '../atoms/Title.svelte';
+	import Logo from '../atoms/Logo.svelte';
 </script>
 
-<body class="bg-gray-700">
-	<div class="pl-6 py-3">
-		<Title title={'HoloTube'} />
+<body>
+	<div class="bg-gray-200 h-16">
+		<div class="pl-4 py-3 flex items-center mb-1 ">
+			<Logo
+				src="https://pbs.twimg.com/profile_images/1090585340081893376/39fZn894.jpg"
+				width="40"
+			/>
+			<div class="pl-3">
+				<Title title={'HoloTube'} />
+			</div>
+		</div>
 	</div>
 </body>

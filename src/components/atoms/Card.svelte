be7b8acba365ad9365e2b3d1ec0b6959f09e063b
Aqua Minato <sweet4182691@gmail.com>
2021-12-21 10:40:31 +0900

    Cardを暫定的に追加

---
+++ src/components/atoms/Card.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let href: string = 'https://google.com';
+	export let title: string = 'Card Title';
+	export let src: string =
+		'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdJ6QkAlJtgAGt16zT690Cq-vdbDqvBAGB5Q&usqp=CAU';
+	export let alt: string = '';
+	export let date: string = '2021/2/21'; // 要命名
+	export let description: string =
+		'ここは説明欄です。ここは説明欄です。ここは説明欄です。ここは説明欄です。ここは説明欄です。ここは説明欄です。ここは説明欄です。';
+</script>
+
+<body>
+	<div class="bg-gray-300 rounded">
+		<a {href}>
+			<h1 class="p-3">{title}</h1>
+			<img {src} {alt} width="100%" />
+			<p class="text-gray-600 pl-3 pt-2">{date}</p>
+			<p class="p-3">{description}</p>
+		</a>
+	</div>
+</body>

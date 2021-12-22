d70cc7a392d33ba1358fb10ffd9091a174ee7c87
Aqua Minato <sweet4182691@gmail.com>
2021-12-23 06:17:55 +0900

    ハンバーガーメニューを使用可能な状態に

--- src/components/organisms/HamburgerMenu.svelte
+++ src/components/organisms/HamburgerMenu.svelte
@@ -5,7 +5,7 @@
 	let icon = 'heroicons-outline:x';
 
 	// export let menuList = {};
-	let menuList = [
+	export let menuList = [
 		{
 			title: 'ぴなちょす',
 			url: 'https://google.com'
@@ -31,19 +31,21 @@
 			url: 'https://google.com'
 		}
 	];
+
+	export let onClick: () => void = () => {};
 </script>
 
 <body>
-	<div class="rounded-md bg-gray-300">
-		<div class="py-6 bg-gray-100 rounded-t-md">
+	<div class="rounded-md bg-blue border border-solid border-gray-200">
+		<div class="py-3 bg-gray-100 rounded-t-md">
 			<div class="flex justify-end pr-5">
-				<MenuButton {icon} onClick={() => {}} />
+				<MenuButton {icon} {onClick} />
 			</div>
 		</div>
 		{#each menuList as menu}
-			<div class="py-2">
+			<div class="m-4">
 				<a href={menu.url}>
-					<p class="pl-4">{menu.title}</p>
+					<p class="pl-4 text-blue-500">{menu.title}</p>
 				</a>
 			</div>
 		{/each}

b2ea0dd1aec533087da1e6777bd62d28f1bfebaf
Aqua Minato <sweet4182691@gmail.com>
2021-12-23 07:38:28 +0900

    データの追加

--- static/data.ts
+++ static/data.ts
@@ -1,14 +1,22 @@
 export const menuList = [
 	{
-		title: 'カバー株式会社',
+		title: 'ホロライブ 公式サイト',
+		url: 'https://www.hololive.tv/'
+	},
+	{
+		title: 'カバー株式会社 公式サイト',
 		url: 'https://cover-corp.com/'
 	},
 	{
-		title: '公式 Youtube',
+		title: 'Youtube 公式チャンネル',
 		url: 'https://www.youtube.com/channel/UCJFZiqLMntJufDCHc6bQixg'
 	},
 	{
-		title: '公式 Twitter',
+		title: 'Twitter 公式アカウント',
 		url: 'https://twitter.com/hololivetv'
+	},
+	{
+		title: 'BOOTH',
+		url: 'https://hololive.booth.pm/'
 	}
 ];

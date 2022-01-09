c7a165132eff44b5ad7ab719f8664f305d9c311b
Aqua Minato <sweet4182691@gmail.com>
2022-01-10 07:01:40 +0900

    Video.svelteの作成

---
+++ src/components/atoms/Video.svelte
@@ -0,0 +1,10 @@
+<iframe
+	class="mx-auto"
+	width="480"
+	height="270"
+	src={'https://www.youtube.com/embed/' + 'w_DV6f9Qf6Q'}
+	title="YouTube video player"
+	frameBorder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+	allowFullScreen
+/>

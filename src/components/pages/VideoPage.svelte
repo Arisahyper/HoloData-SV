5d2c8d706dd95061c51b33b3e2861ace6dea088c
Aqua Minato <sweet4182691@gmail.com>
2022-01-10 07:00:38 +0900

    video表示まで完成

--- src/components/pages/VideoPage.svelte
+++ src/components/pages/VideoPage.svelte
@@ -1,3 +1,23 @@
+<script lang="ts">
+	import Button from '../atoms/Button.svelte';
+</script>
+
 <body>
-	<h1>Video Page</h1>
+	<div class="text-center">
+		<h1>Video Page</h1>
+		<a href="https://www.youtube.com/watch?v=w_DV6f9Qf6Q">
+			<Button text="Youtube" />
+		</a>
+
+		<iframe
+			class="mx-auto"
+			width="480"
+			height="270"
+			src={'https://www.youtube.com/embed/' + 'w_DV6f9Qf6Q'}
+			title="YouTube video player"
+			frameBorder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowFullScreen
+		/>
+	</div>
 </body>

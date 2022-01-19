7bd07aadef0d871a9013a32231eee9fba4dddaa8
Aqua Minato <sweet4182691@gmail.com>
2022-01-20 08:12:58 +0900

    smartcardの機能完成

---
+++ src/components/atoms/SmartCard.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let icon: string =
+		'https://yt3.ggpht.com/DNC5rGjlwA4Ae7rhpPmkv_QdjJPl8AfpQT9NcXTLvPbbriL-Y0Ni-gYfFH8uoiF6Ay1EP9VnZAk=s176-c-k-c0x00ffffff-no-rj';
+	export let href: string = 'https://google.com';
+	export let title: string = 'Card Title';
+	export let src: string =
+		'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdJ6QkAlJtgAGt16zT690Cq-vdbDqvBAGB5Q&usqp=CAU';
+	export let alt: string = '';
+	export let date: string = '2021/2/21 12:00:00'; // 要命名
+	export let name: string = 'さくらみこ';
+	export let isLive: string = '';
+	export let videoUrl: string = '';
+	isLive;
+
+	import { url, liverName, liverIconUrl, startedAt, description } from '../../stores/playVideo';
+	const clickHandler = () => {
+		$url = videoUrl;
+		$liverName = name;
+		$liverIconUrl = icon;
+		$startedAt = date;
+		$description = title;
+	};
+</script>
+
+<body>
+	<div class="bg-gray-100">
+		<a {href} target="">
+			<div class="flex">
+				<img {src} {alt} width="160px" height="90px" on:click={clickHandler} />
+				<div class="flex-wrap">
+					<h1 class="pt-2 pl-2 font-bold text-xs lg:text-base">{name}</h1>
+					<!-- <p class="pl-2 py-0.5 text-gray-600 tracking-wider text-left text-sm">{date}</p> -->
+					<!-- <p class="pl-2 text-sm">{title.slice(1, 10)}</p> -->
+					<p class="pl-2 pt-1 text-xs lg:text-base">{title}</p>
+				</div>
+			</div>
+		</a>
+	</div>
+</body>
---
+++ src/components/templates/SmartCardList.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import Card from '../atoms/Card.svelte';
+	import SmartCard from '../atoms/SmartCard.svelte';
+	import axios from 'axios';
+	import { isOnAir } from '../../stores/isLive';
+
+	async function getHoloList() {
+		const res = await axios.get('https://schedule.hololive.tv/api/list/7');
+		return res.data.dateGroupList;
+	}
+
+	let promise: Promise<any> = getHoloList();
+
+	const url = '/watchpage';
+</script>
+
+<div>
+	<div class="">
+		{#await promise}
+			<div class="text-center">
+				<img
+					width="100%"
+					src="https://c.tenor.com/ugHMd4srOb8AAAAd/minato-aqua-hololive.gif"
+					alt=""
+				/>
+			</div>
+		{:then dateGroupList}
+			{#each [...dateGroupList].reverse() as date}
+				<div class=" border-b border-gray-400 w-1/3 ml-4 pt-3">
+					<h1 class="text-3xl">{date.displayDate}</h1>
+				</div>
+				{#if date.videoList.length == 0}
+					<div class="text-center py-10">
+						<h1>No Video</h1>
+					</div>
+				{/if}
+				<div class="flex flex-wrap">
+					{#if $isOnAir}
+						{#each date.videoList as data}
+							{#if data.isLive == true}
+								<div class="w-full p-1">
+									<SmartCard
+										icon={data.talent.iconImageUrl}
+										name={data.name}
+										href="/watchpage"
+										title={data.title}
+										src={data.thumbnail}
+										date={data.datetime}
+										videoUrl={data.url}
+									/>
+								</div>
+							{/if}
+						{/each}
+					{:else}
+						{#each date.videoList as data}
+							<div class="w-full p-1">
+								<SmartCard
+									icon={data.talent.iconImageUrl}
+									name={data.name}
+									href="/watchpage"
+									title={data.title}
+									src={data.thumbnail}
+									date={data.datetime}
+									videoUrl={data.url}
+								/>
+							</div>
+						{/each}
+					{/if}
+				</div>
+			{/each}
+		{:catch error}
+			<p style="color: red">{error.message}</p>
+		{/await}
+	</div>
+</div>
